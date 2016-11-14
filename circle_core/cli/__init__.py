caf4d7f6d4a5dbeeed5816f7229c54abfe80390c
Y.Tanigawa <yuichiro.tanigawa@glucose.jp>
2016-11-14 16:59:10 +0900

    CLI 環境変数導入 too更新

--- circle_core/cli/__init__.py
+++ circle_core/cli/__init__.py
@@ -5,5 +5,18 @@ import click
 
 
 @click.group()
-def cli_main():
-    pass
+@click.option('--config', envvar='CRCR_CONFIG')
+@click.option('crcr_uuid', '--uuid', envvar='CRCR_UUID')
+@click.pass_context
+def cli_main(ctx, config, crcr_uuid):
+    ctx.obj = {
+        'config': config,
+        'uuid': crcr_uuid,
+    }
+
+
+@cli_main.command('env')
+@click.pass_context
+def cli_env(ctx):
+    click.echo(ctx.obj['config'])
+    click.echo(ctx.obj['uuid'])
