fec155a63f084f76e5e6f68ae298d6eff5462fae
miniohno <tomoki.ohno@glucose.jp>
2016-11-15 16:55:53 +0900

    Fix indentation

--- circle_core/server/ws/__init__.py
+++ circle_core/server/ws/__init__.py
@@ -5,25 +5,25 @@ from tornado.websocket import WebSocketHandler
 # ここからworker達にnanomsgで命令を送る？
 # コネクション毎にインスタンスが生成されている
 class SensorHandler(WebSocketHandler):
-  # 普通に走らせた場合はこのクラス属性に全てのコネクションが入るんだろうが
-  # gunicorn等の上で動かした場合も大丈夫なのかな　よくわかってない
-  waiters = set()
+    # 普通に走らせた場合はこのクラス属性に全てのコネクションが入るんだろうが
+    # gunicorn等の上で動かした場合も大丈夫なのかな　よくわかってない
+    waiters = set()
 
-  def open(self):
-    SensorHandler.waiters.add(self)
-    self.write_message('Greetings from Tornado!')
-    logger.debug('connection opened: %s', self)
+    def open(self):
+        SensorHandler.waiters.add(self)
+        self.write_message('Greetings from Tornado!')
+        logger.debug('connection opened: %s', self)
 
-  def on_message(self, message):
-    for connection in SensorHandler.waiters:
-      connection.write_message(message)
-    logger.debug('message "%s" is sent from %s', message, self)
+    def on_message(self, message):
+        for connection in SensorHandler.waiters:
+            connection.write_message(message)
+            logger.debug('message "%s" is sent from %s', message, self)
 
-  def on_close(self):
-    SensorHandler.waiters.remove(self)
-    logger.debug('connection closed: %s', self)
+    def on_close(self):
+        SensorHandler.waiters.remove(self)
+        logger.debug('connection closed: %s', self)
 
-  def check_origin(self, origin):
-    # デフォルトでCORSチェックが有効になっている
-    # wsta等テストツールから投げる場合はTrueにしておく
-    return True
+    def check_origin(self, origin):
+        # デフォルトでCORSチェックが有効になっている
+        # wsta等テストツールから投げる場合はTrueにしておく
+        return True
