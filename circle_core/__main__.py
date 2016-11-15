8f6e3bca3b7395ce9f6fe80a633b4def23a41583
miniohno <tomoki.ohno@glucose.jp>
2016-11-15 16:32:43 +0900

    Use __main__.py to run the server

---
+++ circle_core/__main__.py
@@ -0,0 +1,4 @@
+from circle_core.cli import cli_main
+
+
+cli_main()
--- circle_core/server/main.py
+++ circle_core/server/main.py
@@ -1,20 +1,20 @@
 from tornado.wsgi import WSGIContainer, WSGIAdapter
 from tornado.web import Application, FallbackHandler
 from tornado.ioloop import IOLoop
-from wui import create_app
-from ws import SensorHandler
+from circle_core.server.wui import create_app
+from circle_core.server.ws import SensorHandler
 
 
 # meinheldとか使う場合はこのファイルに追記していくことになるのかな
 flask_app = create_app()
 tornado_app = Application([
-  (r'/ws/?', SensorHandler),
-  (r'.*', FallbackHandler, {'fallback': WSGIContainer(flask_app)})
+    (r'/ws/?', SensorHandler),
+    (r'.*', FallbackHandler, {'fallback': WSGIContainer(flask_app)})
 ])
 # FIXME: See warning http://www.tornadoweb.org/en/stable/wsgi.html#tornado.wsgi.WSGIContainer
 # これだとパフォーマンスに悪影響が出るっぽいのでどうしようか
 
 
-if __name__ == '__main__':
-  tornado_app.listen(5000)
-  IOLoop.current().start()
+def run_server():
+    tornado_app.listen(5000)
+    IOLoop.current().start()
