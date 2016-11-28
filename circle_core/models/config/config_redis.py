3e03ff38f01900ce848aafb24a0e10c692abe95b
Y.Tanigawa <yuichiro.tanigawa@glucose.jp>
2016-11-28 14:15:07 +0900

    redis_client移動

--- circle_core/models/config/config_redis.py
+++ circle_core/models/config/config_redis.py
@@ -1,17 +1,37 @@
 # -*- coding: utf-8 -*-
 
 # community module
-import redis
+from redis import ConnectionError, Redis
 from six import PY3
 
 # project module
 from .config_base import Config, ConfigError
 from ..device import Device
-from ..redis_client import RedisClient
 from ..schema import Schema
 
 if PY3:
     from typing import Any, Dict
+
+
+class RedisClient(Redis):
+    """Redisクライアント."""
+
+    def parse_response(self, connection, command_name, **options):
+        """Parses a response from the Redis server."""
+        def decoded(word):
+            """bytesはstrに変換する."""
+            return word.decode('utf-8') if isinstance(word, bytes) else word
+
+        response = super(RedisClient, self).parse_response(connection, command_name, **options)
+
+        if PY3:
+            if isinstance(response, bytes):
+                return decoded(response)
+            elif isinstance(response, list):
+                return [decoded(resp) for resp in response]
+            elif isinstance(response, dict):
+                return {decoded(k): decoded(v) for k, v in response.items()}
+        return response
 
 
 class ConfigRedis(Config):
@@ -44,7 +64,7 @@ class ConfigRedis(Config):
         try:
             redis_client = RedisClient.from_url(url_scheme)
             redis_client.ping()
-        except redis.ConnectionError:
+        except ConnectionError:
             raise ConfigError('Cannot connect to Redis server.')
 
         return ConfigRedis(redis_client)
