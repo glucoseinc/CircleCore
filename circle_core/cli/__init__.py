d1cb0be37b2fa5dcc1587cdf49f2c9e0745a23d6
miniohno <tomoki.ohno@glucose.jp>
2016-11-29 18:49:39 +0900

    Remove `crcr server` and `crcr worker` in favor of `crcr run`

--- circle_core/cli/__init__.py
+++ circle_core/cli/__init__.py
@@ -7,11 +7,7 @@ from .bot import cli_bot
 from .cli_main import cli_main
 from .device import cli_device
 from .schema import cli_schema
-from .server import cli_server
-from .worker import cli_worker
 
 cli_main.add_command(cli_device)
 cli_main.add_command(cli_schema)
-cli_main.add_command(cli_server)
-cli_main.add_command(cli_worker)
 cli_main.add_command(cli_bot)
