ac44695aac25c2d6b53f28cc49e3ba521e45aab1
wh11e7rue <wh11e7rue@icloud.com>
2016-12-06 18:59:07 +0900

    Fix 100% CPU usage and refactoring

--- circle_core/cli/cli_main.py
+++ circle_core/cli/cli_main.py
@@ -20,6 +20,8 @@ from circle_core import server
 from circle_core.server import ws, wui
 from circle_core.workers import get_worker
 from .context import ContextObject, ContextObjectError
+from .utils import RestartableProcess
+
 from ..database import Database
 
 
@@ -82,38 +84,20 @@ def cli_main_run(ctx, ws_port, ws_path, wui_port, ipc_socket, workers, database_
     metadata = ctx.obj.metadata
     metadata.database_url = database_url  # とりあえず...
 
-    procs = [RestartableProcess(target=get_worker(worker).run, args=[metadata]) for worker in workers]
+    for worker in workers:
+        RestartableProcess(target=get_worker(worker).run, args=[metadata]).start()
+
     if ws_port == wui_port:
-        procs.append(RestartableProcess(target=server.run, args=[wui_port, metadata]))
+        RestartableProcess(target=server.run, args=[wui_port, metadata]).start()
     else:
-        procs += [
-            RestartableProcess(target=ws.run, args=[ws_path, ws_port]),
-            RestartableProcess(target=wui.create_app(metadata).run, kwargs={'port': wui_port})
-        ]
+        RestartableProcess(target=ws.run, args=[ws_path, ws_port]).start()
+        RestartableProcess(target=wui.create_app(metadata).run, kwargs={'port': wui_port}).start()
 
-    print(
-        'Websocket : ws://{host}:{port}{path}'.format(path=ws_path, port=ws_port, host='127.0.0.1'),
-        file=sys.stderr)
-    print('WebUI : http://127.0.0.1:{port}{path}'.format(path='/', port=ws_port), file=sys.stderr)
-    print('IPC : {}'.format(ctx.obj.ipc_socket), file=sys.stderr)
+    click.echo('Websocket : ws://{host}:{port}{path}'.format(path=ws_path, port=ws_port, host='127.0.0.1'), err=True)
+    click.echo('WebUI : http://127.0.0.1:{port}{path}'.format(path='/', port=ws_port), err=True)
+    click.echo('IPC : {}'.format(ctx.obj.ipc_socket), err=True)
 
-    def kill_children_too(*args):
-        for proc in procs:
-            proc.terminate()  # 子プロセスのSIGTERMハンドラに終了時の処理を書く
-        ctx.abort()
-    signal(SIGTERM, kill_children_too)
-    signal(SIGINT, kill_children_too)
-
-    for proc in procs:
-        proc.start()
-
-    for proc in cycle(procs):
-        if not proc.is_alive():
-            click.echo('PID {} has died. Restarting...'.format(proc.pid))
-            try:
-                proc.start()
-            except RuntimeError:
-                kill_children_too()
+    RestartableProcess.wait_all()
 
 
 @cli_main.command('migrate')
@@ -143,33 +127,3 @@ def cli_main_migrate(ctx, dry_run, database_url):
         db.check_tables()
     else:
         db.migrate()
-
-
-class RestartableProcess:
-    """RestartableProcess.
-
-    :param list args:
-    :param dict kwargs:
-    :param int startedTime:
-    :param Process process:
-    """
-
-    def __init__(self, *args, **kwargs):
-        """constructor."""
-        self.args = args
-        self.kwargs = kwargs
-        self.startedTime = 0
-
-    def __getattr__(self, attr):
-        """delegation."""
-        return getattr(self.process, attr)
-
-    def start(self):
-        """start."""
-        if 10 > time() - self.startedTime:
-            raise RuntimeError('Restarted process has died immediately')
-
-        self.process = Process(*self.args, **self.kwargs)
-        self.process.daemon = True
-        self.process.start()
-        self.startedTime = time()
