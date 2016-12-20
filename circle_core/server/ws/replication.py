a61a42b1bfc8322e0acad009bf3afa76e6c742a1
miniohno <tomoki.ohno@glucose.jp>
2016-12-20 18:27:22 +0900

    Send own schema information

---
+++ circle_core/server/ws/replication.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""他のCircleCoreとの同期."""
+import json
+
+from click import get_current_context
+from tornado.websocket import WebSocketHandler
+
+from ...helpers.nanomsg import Sender
+from ...logger import get_stream_logger
+
+logger = get_stream_logger(__name__)
+
+
+class ReplicationHandler(WebSocketHandler):
+    """スキーマを交換し、まだ相手に送っていないデータを送る
+
+    :param Sender __nanomsg:
+    """
+
+    def open(self):
+        """他のCircleCoreから接続された際に呼ばれる."""
+        logger.debug('Connected to another CircleCore')
+
+    def on_message(self, msg):
+        """センサーからメッセージが送られてきた際に呼ばれる.
+
+        :param unicode message:
+        """
+        action = json.loads(msg)
+        if action['type'] == 'HANDSHAKE':
+            modules = [
+                {
+                    'display_name': module.display_name,
+                    'uuid': module.uuid.hex,
+                    'schema_uuid': module.schema_uuid.hex,
+                    'properties': module.stringified_properties
+                } for module in get_current_context().obj.metadata.modules
+            ]
+            schemas = [
+                {
+                    'display_name': schema.display_name,
+                    'uuid': schema.uuid.hex,
+                    'properties': schema.stringified_properties
+                } for schema in get_current_context().obj.metadata.schemas
+            ]
+            resp = json.dumps({'modules': modules, 'schemas': schemas})
+            self.write_message(resp)
+        elif action['type'] == 'READY':
+            logger.debug('READY')
+
+        logger.debug('message from another circlecore: %r' % msg)
+
+    def on_close(self):
+        """センサーとの接続が切れた際に呼ばれる."""
+        logger.debug('connection closed: %s', self)
+
+    def check_origin(self, origin):
+        """CORSチェック."""
+        # wsta等テストツールから投げる場合はTrueにしておく
+        return True
