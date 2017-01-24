a4f8979d24dcfe5f2ffee1b27e076954dc19d103
Shin ADACHI <shn@glucose.jp>
2017-01-24 11:28:25 +0900

    crcr botでbox-idを指定するように

--- circle_core/cli/bot.py
+++ circle_core/cli/bot.py
@@ -56,7 +56,8 @@ def echo(receive_from, send_to, box_id):
 
 @cli_bot.command()
 @click.option('send_to', '--to', type=click.STRING, default='ws://localhost:5000/module')
-def dummy(send_to):
+@click.option('box_id', '--box-id', type=uuid.UUID)
+def dummy(send_to, box_id):
     """ダミーのデータを投げる.
 
     スキーマ登録: crcr schema add --name dummybot count:int body:text
@@ -65,17 +66,21 @@ def dummy(send_to):
     """
     websocket.enableTrace(True)
     ws = websocket.create_connection(send_to)
+    box_id = str(box_id)
+
     i = 0
     while True:
         i += 1
-        ws.send(json.dumps({'count': i, 'body': "Greetings from a bot"}, indent=2, ensure_ascii=False))
+        d = {'count': i, 'body': "Greetings from a bot", '_box': box_id}
+        ws.send(json.dumps(d, indent=2, ensure_ascii=False))
         click.echo('I sent a message {} times'.format(i))
         sleep(1)
 
 
 @cli_bot.command()
 @click.option('send_to', '--to', type=click.STRING, default='ws://localhost:5000/module')
-def bitcoin(send_to):
+@click.option('box_id', '--box-id', type=uuid.UUID)
+def bitcoin(send_to, box_id):
     """Bitcoinの取引をCircleCoreに送信.
 
     スキーマ登録: crcr schema add --name bitcoinbot address:text btc:float
@@ -86,11 +91,13 @@ def bitcoin(send_to):
     sender = websocket.create_connection(send_to)
     receiver = websocket.create_connection('wss://ws.blockchain.info/inv')
     receiver.send('{"op":"unconfirmed_sub"}')
+    box_id = str(box_id)
     while True:
         res = json.loads(receiver.recv())
         for transaction in res['x']['out']:
             req = json.dumps({
                 'address': transaction['addr'],
                 'btc': transaction['value'] / 10 ** 8,
+                '_box': box_id,
             })
             sender.send(req)
