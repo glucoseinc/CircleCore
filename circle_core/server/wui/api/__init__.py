ff450541368cf0287f262593bcbb497940ffc702
Y.Tanigawa <yuichiro.tanigawa@glucose.jp>
2017-02-03 19:06:11 +0900

    server ReplicationLink API

--- circle_core/server/wui/api/__init__.py
+++ circle_core/server/wui/api/__init__.py
@@ -1,6 +1,7 @@
 from . import cc_infos
 from . import invitations
 from . import modules
+from . import replication_links
 from . import schemas
 from . import users
 from .api import api
---
+++ circle_core/server/wui/api/replication_links.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+"""共有リンク関連APIの実装."""
+
+# community module
+from flask import abort, request
+
+# project module
+from circle_core.cli.utils import generate_uuid
+from circle_core.models import ReplicationLink
+from .api import api
+from ..utils import (
+    api_jsonify, api_response_failure, convert_dict_key_camel_case, convert_dict_key_snake_case, get_metadata,
+    oauth_require_read_schema_scope, oauth_require_write_schema_scope
+)
+
+
+@api.route('/replicas/', methods=['GET', 'POST'])
+def api_replicas():
+    if request.method == 'GET':
+        return _get_replicas()
+    if request.method == 'POST':
+        return _post_replicas()
+    abort(405)
+
+
+@oauth_require_read_schema_scope
+def _get_replicas():
+    metadata = get_metadata()
+
+    response = {
+        'modules': [metadata.denormalize_json_module(module.uuid) for module in metadata.modules],  # 必要な分だけに絞るべきか？
+        'replication_links': [replication_link.to_json() for replication_link in metadata.replication_links]
+    }
+    return api_jsonify(**convert_dict_key_camel_case(response))
+
+
+@oauth_require_write_schema_scope
+def _post_replicas():
+    dic = convert_dict_key_snake_case(request.json)
+    response = {}  # TODO: response形式の統一
+    try:
+        display_name = dic['display_name']
+        message_box_uuids = dic['message_boxes']
+        memo = dic['memo']
+        if len(memo) == 0:
+            memo = None
+    except KeyError:
+        return api_response_failure('key error')
+
+    metadata = get_metadata()
+    replication_link_uuid = generate_uuid(
+        existing=[replication_link.uuid for replication_link in metadata.replication_links]
+    )
+    replication_link = ReplicationLink(replication_link_uuid, message_box_uuids, display_name, memo)
+    metadata.register_replication_link(replication_link)
+    response['result'] = 'success'
+    response['replication_link'] = replication_link.to_json()
+    return api_jsonify(**convert_dict_key_camel_case(response))
+
+
+@api.route('/replicas/<uuid:replication_link_uuid>', methods=['GET'])
+def api_replica(replication_link_uuid):
+    if request.method == 'GET':
+        return _get_replica(replication_link_uuid)
+    abort(405)
+
+
+@oauth_require_read_schema_scope
+def _get_replica(replication_link_uuid):
+    metadata = get_metadata()
+
+    response = {
+        'replication_link': metadata.find_replication_link(replication_link_uuid).to_json()
+    }
+    return api_jsonify(**convert_dict_key_camel_case(response))
