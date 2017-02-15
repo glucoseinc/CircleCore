a4471cc9ba288b7c5312ac36cb1f92536b5a43b6
Shin ADACHI <shn@glucose.jp>
2017-02-15 19:16:19 +0900

    [setup.py] whisperの制約を外す (CIが通らない...)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
     'sqlalchemy>=1.1.4',
     'tornado',
     'websocket-client',
-    'whisper>=0.10.0rc1',
+    'whisper',
 ]
 if PY2:
     install_requires.append('enum34')
