515b051f6b7f91ec7e5606d08e3eae76776ca75d
miniohno <tomoki.ohno@glucose.jp>
2017-02-20 15:59:07 +0900

    Split `crcr bot dummy` as sample/sensor_counter.py

---
+++ sample/sensor_counter.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""一秒ごとに適当なメッセージを投げるセンサー."""
+import json
+from time import sleep
+from uuid import UUID
+
+import click
+import nnpy
+
+
+@click.command()
+@click.option('send_to', '--to', type=click.STRING, default='ipc:///tmp/crcr_request.ipc')
+@click.option('box_id', '--box-id', type=UUID, required=True)
+def counter_bot(send_to, box_id):
+    """一秒ごとに適当なメッセージを投げる.
+
+    スキーマ登録: crcr schema add --name counterbot count:int body:text
+
+    :param str send_to:
+    :param UUID box_id:
+    """
+    socket = nnpy.Socket(nnpy.AF_SP, nnpy.REQ)
+    socket.connect(send_to)
+
+    i = 0
+    while True:
+        i += 1
+
+        msg = json.dumps({
+            'request': 'new_message',
+            'box_id': str(box_id),
+            'payload': {
+                'count': i,
+                'body': "Greetings from a bot",
+            }
+        }, indent=2, ensure_ascii=False)
+        socket.send(msg)
+
+        click.echo('I sent a message {} times'.format(i))
+        sleep(1)
+
+
+if __name__ == '__main__':
+    counter_bot()
