882e0be3ae23545cae5c1743b3d8c3f4c8f9a2e0
Y.Tanigawa <yuichiro.tanigawa@glucose.jp>
2017-02-21 17:03:16 +0900

    server Module Model add moduleProperty

--- circle_core/models/module.py
+++ circle_core/models/module.py
@@ -6,12 +6,90 @@
 import datetime
 
 # community module
+from six import PY3
 import sqlalchemy as sa
 from sqlalchemy import orm
 from sqlalchemy.ext.hybrid import hybrid_property
 
 # project module
 from .base import generate_uuid, GUID, UUIDMetaDataBase
+
+if PY3:
+    from typing import Dict, Iterator, List, Tuple, Union
+
+
+class ModuleProperty(object):
+    """ModuleProperty.
+
+    :param str name: 属性名
+    :param str value: 属性値
+    """
+
+    def __init__(self, name_and_value):
+        """init.
+
+        :param Union[Dict, str] name_and_value: 属性名と属性値
+        """
+        if isinstance(name_and_value, str):
+            if ':' not in name_and_value:
+                raise ValueError('invalid property')
+            self.name, self.value = name_and_value.split(':', 1)
+        elif isinstance(name_and_value, dict):
+            self.name = name_and_value['name']
+            self.value = name_and_value['value']
+
+    def __str__(self):
+        return '{}:{}'.format(self.name, self.value)
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'value': self.value
+        }
+
+
+class ModuleProperties(object):
+    """ModuleProperties.
+
+    :param List _properties: Properties
+    """
+
+    def __init__(self, name_and_values):
+        """init.
+
+        :param Union[str, Tuple, List] name_and_values:
+        """
+
+        self._properties = []
+        if isinstance(name_and_values, str):
+            name_and_values = [name_and_value for name_and_value in name_and_values.split(',')
+                               if len(name_and_value)]
+        if isinstance(name_and_values, (tuple, list)):
+            self._properties = [ModuleProperty(name_and_value) for name_and_value in name_and_values]
+
+    def __iter__(self):
+        """iter.
+
+        :return: イテレータ
+        :rtype: Iterator
+        """
+        return iter(self._properties)
+
+    def __len__(self):
+        """len.
+
+        :return: 長さ
+        :rtype: int
+        """
+        return len(self._properties)
+
+    def __str__(self):
+        """str.
+
+        :return: 文字列表現
+        :rtype: str
+        """
+        return ','.join(str(prop) for prop in self._properties)
 
 
 class Module(UUIDMetaDataBase):
@@ -29,6 +107,7 @@ class Module(UUIDMetaDataBase):
     uuid = sa.Column(GUID, primary_key=True)
     cc_uuid = sa.Column(GUID, sa.ForeignKey('cc_informations.uuid', name='fk_modules_cc_uuid'), nullable=False)
     display_name = sa.Column(sa.String(255), nullable=False, default='')
+    _properties = sa.Column('properties', sa.Text, nullable=False, default='')
     _tags = sa.Column('_tags', sa.Text, nullable=False, default='')
     memo = sa.Column(sa.Text, nullable=False, default='')
     created_at = sa.Column(sa.DateTime, nullable=False, default=datetime.datetime.utcnow)
@@ -51,8 +130,13 @@ class Module(UUIDMetaDataBase):
 
     def __init__(self, **kwargs):
         """init.
-        """
 
+        :param Dict kwargs: キーワード引数
+        """
+        if 'properties' in kwargs:
+            properties = kwargs['properties']
+            if not isinstance(properties, ModuleProperties):
+                kwargs['properties'] = ModuleProperties(properties)
         if 'tags' in kwargs:
             kwargs['_tags'] = ','.join(self.to_tags_list(kwargs.pop('tags')))
 
@@ -97,6 +181,16 @@ class Module(UUIDMetaDataBase):
                 raise ValueError('invalid tag, `{}`'.format(tag))
         return tags
 
+    @hybrid_property
+    def properties(self):
+        return ModuleProperties(self._properties)
+
+    @properties.setter
+    def properties(self, properties):
+        if not isinstance(properties, ModuleProperties):
+            properties = ModuleProperties(properties)
+        self._properties = str(properties)
+
     def to_json(self, with_boxes=False, with_schema=False):
         """このモデルのJSON表現を返す.
 
@@ -110,6 +204,7 @@ class Module(UUIDMetaDataBase):
             'messageBoxUuids': [str(box.uuid) for box in self.message_boxes],
             'displayName': self.display_name,
             'tags': [tag for tag in self.tags],
+            'properties': [prop.to_json() for prop in self.properties],
             'memo': self.memo,
         }
 
@@ -121,6 +216,8 @@ class Module(UUIDMetaDataBase):
     def update_from_json(self, jsonobj, with_boxes=False):
         self.display_name = jsonobj.get('displayName', self.display_name)
         self.tags = jsonobj.get('tags', self.tags)
+        if 'properties' in jsonobj:
+            self.properties = ModuleProperties(jsonobj['properties'])
         self.memo = jsonobj.get('memo', self.memo)
 
         if with_boxes and 'messageBoxes' in jsonobj:
