f5e1578eb5f1de6b46018a23c7fcd34932188f3b
Shin ADACHI <shn@glucose.jp>
2017-03-09 18:33:33 +0900

    httpsでログインが出来ない問題を修正

--- circle_core/web/__init__.py
+++ circle_core/web/__init__.py
@@ -2,12 +2,12 @@
 """WebUI."""
 
 
-def create_app(core, base_url=None, ws_port=None):
+def create_app(core, base_url=None, ws_port=None, is_https=False):
     """App factory.
 
     :param Optional[Union[MetadataIniFile, MetadataRedis]] metadata: Metadata
     :rtype: Flask
     """
     from .app import CCWebApp
-    app = CCWebApp(core, base_url, ws_port)
+    app = CCWebApp(core, base_url, ws_port, is_https=is_https)
     return app
