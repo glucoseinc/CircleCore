77c414bbe309016c9034954541ba68234751c0be
Shin ADACHI <shn@glucose.jp>
2018-11-22 01:08:59 +0900

    postcss-preset-envの設定変更

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,12 @@ module.exports = ({env, file, options}) => {
   let plugins = [
     require('postcss-import'),
     require('postcss-mixins'),
-    require('postcss-preset-env')(),
+    require('postcss-preset-env')({
+      stage: 1,
+      features: {
+        'nesting-rules': true
+      }
+    }),
   ]
 
   if(env === 'staging' || env === 'production') {
