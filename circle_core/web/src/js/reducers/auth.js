7dedd3c3d9699561e0538e9c15d8496bc1914414
oblique1121 <oblique1121@hotmail.com>
2019-01-08 10:36:16 +0900

    [BROKEN]fix auth initial state

--- circle_core/web/src/js/reducers/auth.js
+++ circle_core/web/src/js/reducers/auth.js
@@ -1,27 +1,18 @@
-import {LOCATION_CHANGE} from 'connected-react-router'
 import {handleActions} from 'redux-actions'
 
 import {actionTypes} from 'src/actions'
 import CCAPI from 'src/api'
 import {oauthToken} from 'src/Authorization'
 
+CCAPI.setToken(oauthToken)
+oauthToken.load()
+
 const initialState = {
   token: oauthToken,
   tokenLoaded: false,
-  tokenIsValid: false,
+  tokenIsValid: oauthToken.isValid(),
 }
 const entities = handleActions({
-  [LOCATION_CHANGE]: (state, action) => {
-    // 起動時にTokenを読み込む
-    if (!state.tokenLoaded) {
-      CCAPI.setToken(state.token)
-      state.token.load()
-      return {...state, tokenLoaded: true, tokenIsValid: state.token.isValid()}
-    }
-
-    return state
-  },
-
   [actionTypes.auth.loginSucceeded]: (state, action) => {
     // eslint-disable-next-line camelcase
     const {access_token, refresh_token, scope} = action.payload
