2b35f886d3ebebd7d3f07fc3cffd3dff2edc4f35
Shin ADACHI <shn@glucose.jp>
2019-01-12 16:03:24 +0900

    UserのTokenを表示 & 更新

--- circle_core/web/src/js/components/UserEditPaper/UserInfoEditComponent.jsx
+++ circle_core/web/src/js/components/UserEditPaper/UserInfoEditComponent.jsx
@@ -6,81 +6,125 @@ import TextField from 'material-ui/TextField'
 
 import DisplayNameTextField from 'src/components/commons/DisplayNameTextField'
 import WorkTextField from 'src/components/commons/WorkTextField'
+import LabelWithCopyButton from 'src/containers/bases/LabelWithCopyButton'
+import CCRaisedButton from 'src/components/bases/CCRaisedButton'
 
 
 /**
-* UserInfo編集コンポーネント
-*/
-class UserInfoEditComponent extends React.Component {
-  static propTypes = {
-    user: PropTypes.object.isRequired,
-    errors: PropTypes.object,
-    onUpdate: PropTypes.func,
-    canChangePermission: PropTypes.bool,
+ * UserInfo編集コンポーネント
+ *
+ * @param {props} props
+ * @return {React.Compoonent}
+ */
+const UserInfoEditComponent = (props) => {
+  const {
+    canChangePermission,
+    source,
+    current,
+    errors = {},
+    onUpdate,
+    onRenewTokenRequested,
+  } = props
+
+  const styles = {
+    root: {
+      display: 'flex',
+      flexFlow: 'column nowrap',
+      width: '100%',
+    },
+
+    tokenFormGroup: {
+      marginTop: '1ex',
+    },
+
+    inputLabel: {
+      fontSize: '0.75rem',
+    },
+
+    noToken: {
+      color: 'rgba(0, 0, 0, 0.3)',
+      flex: 'auto',
+    },
+
+    token: {
+      flex: 'auto',
+    },
+
+    tokenRow: {
+      display: 'flex',
+    },
+
+    renewButton: {
+      marginLeft: '8px',
+      minWidth: '120px',
+    },
   }
 
-  static defaultProps = {
-    canChangePermission: true,
-  }
+  const accountErrorText = errors.account
 
-  /**
-   * @override
-   */
-  render() {
-    const {
-      canChangePermission,
-      user,
-      errors = {},
-      onUpdate,
-    } = this.props
+  return (
+    <div style={styles.root}>
+      <DisplayNameTextField
+        obj={current}
+        floatingLabelText="アカウント"
+        errorText={accountErrorText}
+        onChange={(e) => onUpdate(current.updateDisplayName(e.target.value))}
+      />
 
-    const style = {
-      root: {
-        display: 'flex',
-        flexFlow: 'column nowrap',
-        width: '100%',
-      },
-    }
+      <Checkbox
+        label="管理権限"
+        disabled={!canChangePermission}
+        style={{margin: '12px 0'}}
+        checked={current.isAdmin}
+        onCheck={(e, v) => onUpdate(current.updateIsAdmin(v))}
+      />
 
-    const accountErrorText = errors.account
-
-    return (
-      <div style={style.root}>
-        <DisplayNameTextField
-          obj={user}
-          floatingLabelText="アカウント"
-          errorText={accountErrorText}
-          onChange={(e) => onUpdate(user.updateDisplayName(e.target.value))}
-        />
-
-        <Checkbox
-          label="管理権限"
-          disabled={!canChangePermission}
-          style={{margin: '12px 0'}}
-          checked={user.isAdmin}
-          onCheck={(e, v) => onUpdate(user.updateIsAdmin(v))}
-        />
-
-        <WorkTextField
-          obj={user}
-          onChange={(e) => onUpdate(user.updateWork(e.target.value))}
-        />
-        <TextField
-          floatingLabelText="メールアドレス"
-          fullWidth={true}
-          value={user.mailAddress}
-          onChange={(e) => onUpdate(user.updateMailAddress(e.target.value))}
-        />
-        <TextField
-          floatingLabelText="電話番号"
-          fullWidth={true}
-          value={user.telephone}
-          onChange={(e) => onUpdate(user.updateTelephone(e.target.value))}
-        />
+      <WorkTextField
+        obj={current}
+        onChange={(e) => onUpdate(current.updateWork(e.target.value))}
+      />
+      <TextField
+        floatingLabelText="メールアドレス"
+        fullWidth={true}
+        value={current.mailAddress}
+        onChange={(e) => onUpdate(current.updateMailAddress(e.target.value))}
+      />
+      <TextField
+        floatingLabelText="電話番号"
+        fullWidth={true}
+        value={current.telephone}
+        onChange={(e) => onUpdate(current.updateTelephone(e.target.value))}
+      />
+      <div style={styles.tokenFormGroup}>
+        <div style={styles.inputLabel}>トークン</div>
+        <div style={styles.tokenRow}>
+          {source.token ?
+            <LabelWithCopyButton
+              label={source.token}
+              style={styles.token}
+              messageWhenCopying="トークンをコピーしました"
+            /> :
+            <div style={styles.noToken}>トークンはありません</div>
+          }
+          <CCRaisedButton
+            style={styles.renewButton}
+            label={source.token ? '再生成する' : '生成する'} onClick={onRenewTokenRequested}
+          />
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
+UserInfoEditComponent.propTypes = {
+  canChangePermission: PropTypes.bool,
+  source: PropTypes.object.isRequired,
+  current: PropTypes.object.isRequired,
+  errors: PropTypes.object,
+  onUpdate: PropTypes.func,
+  onRenewTokenRequested: PropTypes.func,
+}
+UserInfoEditComponent.defaultProps = {
+  canChangePermission: true,
+}
 
 export default UserInfoEditComponent
